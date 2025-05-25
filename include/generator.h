554f33eebde9d78e62606309908d8140edb28f57
otilia <ionelotilia.r@gmail.com>
2025-05-25 12:48:04 +0300

    sadasd

--- include/generator.h
+++ include/generator.h
@@ -1,84 +1,69 @@
 #ifndef GENERATOR_H
 #define GENERATOR_H
+
 #include <ctime>
 #include <map>
 #include <memory>
-#include "labirint.h"
 #include <random>
+#include "labirint.h"
+
 class labirint;
+
 template<typename T>
 class generator {
     labirint &lab;
-    std::map<std::pair<int,int>,T*> obiect ;
+    std::map<std::pair<int,int>, std::unique_ptr<T>> obiect;
     int nr_obiecte;
 public:
-    generator(labirint& lab,int nr);
-    ~generator();
+    generator(labirint& lab, int nr);
+    ~generator() = default;
     void plaseaza_obiecte();
-    void sterge_obiecte(int x,int y);
-    T* get_obiect(int x,int y);
-    bool obiect_in_cale(int x,int y);
-
+    void sterge_obiecte(int x, int y);
+    T* get_obiect(int x, int y);
+    bool obiect_in_cale(int x, int y);
 };
 
 template<typename T>
-generator<T>:: generator(labirint& lab,int nr):lab(lab),nr_obiecte(nr)
-{};
-
-template<typename T>
-generator<T>::~generator() {
-    for (auto& [poz, ob] : obiect)
-        delete ob;
-    obiect.clear();
-}
+generator<T>::generator(labirint& lab, int nr) : lab(lab), nr_obiecte(nr) {}
 
 template<typename T>
 void generator<T>::plaseaza_obiecte() {
     int i = 0;
-
     while (i < nr_obiecte) {
-        int new_x = rand()%lab.get_dimensiuni().first;
-        int new_y = rand()%lab.get_dimensiuni().second;
-
-        if (lab.drum_liber(new_x,new_y) && lab.drum_liber(new_x,new_y+1) && lab.drum_liber(new_x,new_y-1)) {
-            T* ob = new T();
-            ob->set_pozitie(new_x,new_y);
-            obiect[{new_x,new_y}] = ob;
+        int new_x = rand() % lab.get_dimensiuni().first;
+        int new_y = rand() % lab.get_dimensiuni().second;
+        if (lab.drum_liber(new_x, new_y) && lab.drum_liber(new_x, new_y + 1) && lab.drum_liber(new_x, new_y - 1)) {
+            auto ob = std::make_unique<T>();
+            ob->set_pozitie(new_x, new_y);
             lab.get_harta()[new_x][new_y] = ob->print_caracter();
-
-            ///TODO:metoda la labirint de adaugare obiect, getterele returneaza CONST
+            obiect[{new_x, new_y}] = std::move(ob);
             i++;
         }
     }
 }
 
 template<typename T>
-void generator<T>::sterge_obiecte(int x,int y) {
-    auto it = obiect.find({x, y});
-    if (it != obiect.end()) {
-        delete it->second;
-        obiect.erase(it);
-        lab.get_harta()[x][y] = '_';
-    }
+void generator<T>::sterge_obiecte(int x, int y) {
+    obiect.erase({x, y});
+    lab.get_harta()[x][y] = '_';
 }
 
 template<typename T>
-T* generator<T>::get_obiect(int x,int y) {
+T* generator<T>::get_obiect(int x, int y) {
     auto it = obiect.find({x, y});
     if (it != obiect.end())
-        return it->second;
+        return it->second.get();
     return nullptr;
 }
 
 template<typename T>
-bool generator<T>::obiect_in_cale(int x,int y) {
-    return get_obiect(x,y)!=nullptr;
+bool generator<T>::obiect_in_cale(int x, int y) {
+    return get_obiect(x, y) != nullptr;
 }
 
 template<typename T, typename Baza>
 int numara_obiecte(const std::vector<std::shared_ptr<Baza>>& obiecte) {
     int nr = 0;
-
     for (auto& ob : obiecte) {
         if (std::dynamic_pointer_cast<T>(ob))
             nr++;
